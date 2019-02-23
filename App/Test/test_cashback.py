fe35093fd0a35cee0e363deaa2af6b7d26630dab
ayrat-l <kaz90@bk.ru>
2019-02-23 22:45:00 +0300

    in .travis.yml rename python 3.6 -> 3.7

--- App/Test/test_cashback.py
+++ App/Test/test_cashback.py
@@ -4,3 +4,8 @@ def test_cashback_under_limit():
     result = cashback(1_000)
 
     assert 50 == result
+
+def cashback_over_limit():
+    result = cashback(1_000_000)
+
+    assert 3_000 == result
